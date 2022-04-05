febd75b043dbccd8355be253a4e3a718ed16a480
GeorgeDenis <george.duluta@yahoo.com>
2022-04-05 11:13:56 +0300

    Laborator7Tema1

---
+++ Laborator7/Laborator7Tema1/Laborator7Tema1/Source.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <stdio.h>
+
+double operator"" _Kelvin(unsigned long long x) {
+    return (double) ((x - 273.15));
+}
+
+double operator"" _Fahrenheit(unsigned long long x) {
+    return (double) ((x - 32) / 1.8);
+}
+
+int main() {
+    float a = 300_Kelvin;
+    float b = 120_Fahrenheit;
+
+    printf("%f\n", a);
+    printf("%f\n", b);
+
+    return 0;
+}
