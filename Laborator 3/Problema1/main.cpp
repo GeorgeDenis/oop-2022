04c00696354d7b6b1ebf0820c199e9b10356e900
Galatianu Mihai <mgalatianu@yahoo.ro>
2023-03-26 16:06:03 +0300

    Am testat
    Descrierea

--- Laborator 3/Problema1/main.cpp
+++ Laborator 3/Problema1/main.cpp
@@ -2,6 +2,7 @@
 #include "Math.h"
 
 void Exemple() {
+    //AM MODIFICAT AICIgi
     int a = 4,b = 8, c = 16;
     double A = 4.52, B = 7.383, C = 22.76;
     int n = 5, la = 42, lb = 9, lc = 32, ld = 58, le = 142;
